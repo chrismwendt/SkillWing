011afb153c706e9af954d60d2d3762fa4db524ba
Chris Wendt <chrismwendt@gmail.com>
2014-01-01 01:57:50 -0700

    Possibly return an error from the history function

--- app.js
+++ app.js
@@ -18,16 +18,22 @@ app.get('/', function(request, response) {
 var history = function(name, callback) {
     db.Item.findOne({name: name}, function(error, result) {
         if (error || !result) {
-            console.log('Could not get history for ' + name);
+            callback('Could not get history for ' + name);
+            return;
         }
-        callback(utility.pricesToCSV(result.history));
+        callback(null, utility.pricesToCSV(result.history));
     });
 }
 
 // extract 'Yew logs' from '/Yew logs.csv'
 app.get('/:name([^.]*).csv', function(request, response) {
     var name = request.params.name;
-    history(name, function(csv) {
+    history(name, function(error, csv) {
+        if (error) {
+            console.log(error);
+            response.send('');
+            return;
+        }
         response.send(csv);
     });
 });
