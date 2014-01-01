d9d1c63ce604ecca5b138267df0e42e4c58e5eac
Chris Wendt <chrismwendt@gmail.com>
2014-01-01 03:09:40 -0700

    Make the request queue a factory
    Each time require('request-queue')() is called, a new request queue is created.

--- source/request-queue.js
+++ source/request-queue.js
@@ -2,36 +2,47 @@ var __ = require('underscore');
 var request = require('request');
 //request = require('./request-proxy'); // for development without an Internet connection
 
-exports.minDelay = 1/16*1000;
-exports.maxDelay = 16*1000;
-exports.enqueue = function(urls, callback) {
-    urls = __.union(urls, urls);
-    __.each(urls, function(url) {
-        queue.push({'url': url, 'callback': callback});
-    });
-    if (queue.length == urls.length) scheduleNext();
-};
-exports.drain = undefined;
+module.exports = function() {
+    var requestQueue = {
+        drain: undefined,
+    };
 
-var queue = [];
-var delay = exports.minDelay;
-var scheduleNext = function() {
-    __.delay(__(request).partial(queue[0].url, __(handler).partial(Date.now())), delay);
-};
-var handler = function(startTime, error, response) {
-    if (error || !response.body) { 
-        //console.log('F');
-        //console.log(error);
-        delay = Math.min(delay*2, exports.maxDelay);
-        scheduleNext();
-        return;
-    }
+    var minDelay = 1/16*1000;
+    var maxDelay = 16*1000;
+    var queue = [];
+    var delay = minDelay;
 
-    //console.log('.');
-    queue[0].callback(response, startTime);
-    queue.shift();
+    var enqueue = function(urls, callback) {
+        urls = __.union(urls, urls);
+        __.each(urls, function(url) {
+            queue.push({'url': url, 'callback': callback});
+        });
+        if (queue.length == urls.length) scheduleNext();
+    };
 
-    delay = Math.max(delay/2, exports.minDelay);
-    if (!__.isEmpty(queue)) scheduleNext();
-    else if (exports.drain) exports.drain();
+    var scheduleNext = function() {
+        __.delay(__(request).partial(queue[0].url, __(handler).partial(Date.now())), delay);
+    };
+
+    var handler = function(startTime, error, response) {
+        if (error || !response.body) { 
+            // console.log('F');
+            // console.log(error);
+            delay = Math.min(delay*2, maxDelay);
+            scheduleNext();
+            return;
+        }
+
+        //console.log('.');
+        queue[0].callback(response, startTime);
+        queue.shift();
+
+        delay = Math.max(delay/2, minDelay);
+        if (!__.isEmpty(queue)) scheduleNext();
+        else if (requestQueue.drain) requestQueue.drain();
+    };
+
+    requestQueue.enqueue = enqueue;
+
+    return requestQueue;
 };
