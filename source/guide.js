a623fe06013b9b6801a64d8136f3a5e19f75222a
Chris Wendt <chrismwendt@gmail.com>
2014-01-14 12:25:55 -0700

    Include time value and gp/h in guide calculation

--- source/guide.js
+++ source/guide.js
@@ -37,53 +37,60 @@ var gph = function(method, callback) {
     });
 };
 
+var agpxp = function(timeValue, xp, gp) {
+    // adjusted gp/xp = gp/xp - mm/h / xp/h           -> linear
+    return gp / xp - timeValue / xp;
+}
+
 exports.get = function(skill, timeValue, callback) {
+    timeValue = Number(timeValue);
     var skillMethods = _.filter(methods, function(method) {
         return skill in method.xp;
     });
 
-    var rawList = _.map(_.range(1,100), function(level) {
-        return _.chain(skillMethods)
-            .filter(function(method) {
-                return method.requirements[skill] == undefined ||
-                    level >= method.requirements[skill];
-            })
-            .max(function(method) {
-                return method.xp[skill];
-            })
-            .value();
-    });
+    async.map(skillMethods, function(method, cb) {
+        gph(method, function(gp) {
+            method.gp = gp;
+            cb(null, method);
+        });
+    }, function(error, results) {
+        skillMethods = results;
 
-    var groups = _.chain(groupBy(rawList, function(a, b) {
-            return a.name == b.name;
-        }))
-        .foldl(function(acc, group) {
-            acc.acc.push({
-                start: acc.n,
-                end: acc.n + group.length - 1,
-                group: group[0]
-            });
-            acc.n += group.length;
-            return acc;
-        }, {n: 1, acc: []})
-        .value()
-        .acc;
+        var rawList = _.map(_.range(1,100), function(level) {
+            return _.chain(skillMethods)
+                .filter(function(method) {
+                    return method.requirements[skill] == undefined ||
+                        level >= method.requirements[skill];
+                })
+                .max(function(method) {
+                    return agpxp(timeValue, method.xp[skill], method.gp);
+                })
+                .value();
+        });
 
-    async.map(groups, function(group, cb) {
-        gph(group.group, function(g) {
-            cb(null, {
+        var groups = _.chain(groupBy(rawList, function(a, b) {
+                return a.name == b.name;
+            }))
+            .foldl(function(acc, group) {
+                acc.acc.push({
+                    start: acc.n,
+                    end: acc.n + group.length - 1,
+                    group: group[0]
+                });
+                acc.n += group.length;
+                return acc;
+            }, {n: 1, acc: []})
+            .value()
+            .acc;
+
+        callback(_.map(groups, function(group) {
+            return {
                 levels: group.start + '-' + group.end,
                 method: group.group.name,
                 xp: group.group.xp[skill],
-                gp: g
-            });
-        });
-    }, function(error, results) {
-        if (error) {
-            console.log(error);
-        }
-
-        callback(results);
+                gp: group.group.gp
+            };
+        }));
     });
 };
 
