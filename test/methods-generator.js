3ea5d5f0bc63960316d1f0b7e7e52b2665f26b4a
Chris Wendt <chrismwendt@gmail.com>
2014-01-14 12:23:25 -0700

    Remove the script to generate training methods
    One should enter the data instead of writing a script to generate the data.

--- test/methods-generator.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs');
-var __ = require('underscore');
-var expect = JSON.parse(fs.readFileSync('data/methods.json'));
-var got = JSON.parse(fs.readFileSync('data/methods.json.new'));
-console.log(__.isEqual(got, expect));
