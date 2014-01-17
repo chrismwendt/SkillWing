4b57291e224a7cebb8e7f077537a6949085474ad
Chris Wendt <chrismwendt@gmail.com>
2014-01-17 04:15:56 -0700

    Refactor the historian to avoid a query stream
    For some reason, the stream was being closed wihout error after 584 items.

--- source/historian.js
+++ source/historian.js
@@ -9,7 +9,42 @@ var getGraphURL = function(id) {
     return 'http://services.runescape.com/m=itemdb_rs/api/graph/' + id + '.json';
 }
 
-module.exports = function(db) {
+var db;
+
+var processItem = function(item, callback) {
+    db.Item.findOne({id: item.id}, function(error, item) {
+        requestQueue.enqueue(getGraphURL(item.id), function(response, response_timestamp) {
+            var history;
+            try {
+                history = JSON.parse(response.body).daily;
+            } catch (e) {
+                console.log('Fail on ' + item.name);
+                console.log('Could not parse string ' + response.body);
+                console.log(e);
+                callback();
+                return;
+            }
+
+            item.history = []; // clear history to retard database growth
+            item.history = _.map(history, function(price, timestamp) {
+                return {
+                    timestamp: Number(timestamp),
+                    price: price
+                };
+            });
+
+            item.save(function(error) {
+                if (error) {
+                    console.log(error);
+                }
+                callback();
+            });
+        });
+    });
+}
+
+module.exports = function(_db) {
+    db = _db;
     async.forever(function(callback_original) {
         callback = function(error) {
             if (error) {
@@ -21,39 +56,9 @@ module.exports = function(db) {
             setTimeout(callback_original, 1000*3600*24); // update again in 24 hours
         }
 
-        var stream = db.Item.find().stream();
-
-        stream.on('data', function(item) {
-            stream.pause();
-            requestQueue.enqueue(getGraphURL(item.id), function(response, response_timestamp) {
-                var history;
-                try {
-                    history = JSON.parse(response.body).daily;
-                } catch (e) {
-                    console.log('Could not parse string ' + response.body);
-                    console.log(e);
-                    stream.resume();
-                    return;
-                }
-
-                item.history = []; // clear history to retard database growth
-                item.history = _.map(history, function(price, timestamp) {
-                    return {
-                        timestamp: Number(timestamp),
-                        price: price
-                    };
-                });
-
-                item.save(function(error) {
-                    if (error) {
-                        console.log(error);
-                    }
-                    stream.resume();
-                });
-            });
+        db.Item.find({}, {_id:0, id:1}).lean().exec(function(error, items) {
+            async.eachSeries(items, processItem, callback);
         });
-
-        stream.on('close', callback);
     }, function(error) {
         console.log(error);
     });
